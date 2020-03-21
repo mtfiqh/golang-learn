80590464bb82b1a9e51e8a30f2834604a59cc2d2
Muhammad Taufiq Hidayat <mtfiqh@gmail.com>
2020-03-22 02:30:42 +0700

    variable const

---
+++ bab10/konstanta.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const tapi pake typedata
+	const firstName string = "Taufiq"
+	// const gapake typedata, gaperlu := cukup = aja
+	const userName = "mtfiqh"
+	fmt.Println("const firstName String = ", firstName)
+	fmt.Println("const userName tanpa typedata = ", userName)
+}
